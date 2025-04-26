0189a1634355a3fa51a4c35f31c22d2d47c19200
Nam Phan <namph.tech@gmail.com>
2025-04-26 14:00:56 +0700

    feat: frontend ~ switch back to development mode

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -6,8 +6,8 @@ import type { Metadata } from 'next';
 import { cookies } from 'next/headers';
 
 export const metadata: Metadata = {
-  title: 'Next Shadcn Dashboard Starter',
-  description: 'Basic dashboard with Next.js and Shadcn'
+  title: 'Task Management',
+  description: 'Task Management Dashboard',
 };
 
 export default async function DashboardLayout({
