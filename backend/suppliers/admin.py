46a38eaebfe0b956ade952650917a519c8ced793
Nam Phan <namph.tech@gmail.com>
2025-04-26 15:28:34 +0700

    chore: BE ~ remove some redundant code

--- backend/suppliers/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.apps import AppConfig
-
-
-class SuppliersConfig(AppConfig):
-  default_auto_field = "django.db.models.BigAutoField"
-# Register your models here.
--- backend/users/models.py
+++ backend/users/models.py
@@ -15,85 +15,20 @@ from twilio.rest import Client
 User = get_user_model()
 
 
-class PhoneNumber(models.Model):
-    user = models.OneToOneField(User, related_name="phone", on_delete=models.CASCADE)
-    phone_number = PhoneNumberField(unique=True)
-    security_code = models.CharField(max_length=120)
-    is_verified = models.BooleanField(default=False)
-    sent = models.DateTimeField(null=True)
+class Role(models.Model):
+    EMPLOYER = "employer"
+    EMPLOYEE = "employee"
+    ROLE_CHOICES = [
+        (EMPLOYER, "Employer"),
+        (EMPLOYEE, "Employee"),
+    ]
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    class Meta:
-        ordering = ("-created_at",)
-
-    def __str__(self):
-        return self.phone_number.as_e164
-
-    def generate_security_code(self):
-        """
-        Returns a unique random `security_code` for given `TOKEN_LENGTH` in the settings.
-        Default token length = 6
-        """
-        token_length = getattr(settings, "TOKEN_LENGTH", 6)
-        return get_random_string(token_length, allowed_chars="0123456789")
-
-    def is_security_code_expired(self):
-        expiration_date = self.sent + datetime.timedelta(
-            minutes=settings.TOKEN_EXPIRE_MINUTES
-        )
-        return expiration_date <= timezone.now()
-
-    def send_confirmation(self):
-        twilio_account_sid = settings.TWILIO_ACCOUNT_SID
-        twilio_auth_token = settings.TWILIO_AUTH_TOKEN
-        twilio_phone_number = settings.TWILIO_PHONE_NUMBER
-
-        self.security_code = self.generate_security_code()
-
-        # print(
-        #     f'Sending security code {self.security_code} to phone {self.phone_number}')
-
-        if all([twilio_account_sid, twilio_auth_token, twilio_phone_number]):
-            try:
-                twilio_client = Client(twilio_account_sid, twilio_auth_token)
-                twilio_client.messages.create(
-                    body=f"Your activation code is {self.security_code}",
-                    to=str(self.phone_number),
-                    from_=twilio_phone_number,
-                )
-                self.sent = timezone.now()
-                self.save()
-                return True
-            except TwilioRestException as e:
-                print(e)
-        else:
-            print("Twilio credentials are not set")
-
-    def check_verification(self, security_code):
-        if (
-            not self.is_security_code_expired()
-            and security_code == self.security_code
-            and not self.is_verified
-        ):
-            self.is_verified = True
-            self.save()
-        else:
-            raise NotAcceptable(
-                _(
-                    "Your security code is wrong, expired or this phone is verified before."
-                )
-            )
-
-        return self.is_verified
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="avatar", blank=True)
-    bio = models.CharField(max_length=200, blank=True)
-
+    user = models.OneToOneField(User, related_name="role")
+    role = models.CharField(
+        max_length=50,
+        choices=ROLE_CHOICES,
+        blank=False,
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
